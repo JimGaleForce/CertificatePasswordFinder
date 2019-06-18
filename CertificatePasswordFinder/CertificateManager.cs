19cca8f75dcf15ad71f8d718920cd31934580920
Jim Gale <jimgaleforce@gmail.com>
2019-06-17 23:39:35 -0700

    Added finder with Console app

---
+++ CertificatePasswordFinder/CertificateManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CertificatePasswordFinder
+{
+    public class CertificateManager
+    {
+        private string path { get; set; }
+
+        public CertificateManager(string certOrPfxPath)
+        {
+            this.path = certOrPfxPath;
+        }
+
+        public bool TryPassword(string password)
+        {
+            try
+            {
+                var certificate = new X509Certificate2(path, password);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public string PermutatePasswords(string[] segments)
+        {
+            return PermutatePasswords(segments, "", 0);
+        }
+
+        private string PermutatePasswords(string[] parts, string text, int num)
+        {
+            if (num == parts.Length)
+            {
+                if (TryPassword(text))
+                {
+                    return text;
+                }
+
+                return "";
+            }
+
+            var seg = parts[num].Split(' ');
+
+            for (var i = 0; i < seg.Length; i++)
+            {
+                var pw = PermutatePasswords(parts, text + seg[i], num + 1);
+                if (!string.IsNullOrWhiteSpace(pw))
+                {
+                    return pw;
+                }
+
+                if (seg[i].Length > 0 && char.IsLower(seg[i][0]))
+                {
+                    pw = PermutatePasswords(parts, text + seg[i][0].ToString().ToUpper() + seg[i].Substring(1), num + 1);
+                    if (!string.IsNullOrWhiteSpace(pw))
+                    {
+                        return pw;
+                    }
+
+                    pw = PermutatePasswords(parts, text + seg[i].ToUpper(), num + 1);
+                    if (!string.IsNullOrWhiteSpace(pw))
+                    {
+                        return pw;
+                    }
+                }
+            }
+
+            return "";
+        }
+    }
+}
---
+++ FinderConsole/Program.cs
@@ -0,0 +1,54 @@
+﻿using CertificatePasswordFinder;
+using System;
+using System.Collections.Generic;
+
+namespace FinderConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Certificate Password Finder v1.00");
+            Console.WriteLine("");
+            Console.WriteLine(" Let's say you think the password starts with either aaa, bbb, ccc, or ddd,");
+            Console.WriteLine(" then continues with 111, 222, 333, or 444,");
+            Console.WriteLine(" then ends with qqq, www, or eee.");
+            Console.WriteLine(" To try them all, enter 'aaa bbb ccc ddd' as segment #1, '111 222 333 444' as #2, 'qqq www eee' as #3 without quotes.");
+            Console.WriteLine("");
+
+            Console.Write("Enter path of cert/pfx : ");
+            var path = Console.ReadLine();
+
+            if (path.StartsWith("\""))
+            {
+                path = path.Substring(1, path.Length - 2);
+            }
+
+            var list = new List<string>();
+
+            Console.WriteLine("Enter each segment with words separated by spaces (i.e. this that those)");
+            Console.WriteLine("Enter a blank line to end the segments");
+
+            var line = "x";
+            while (!string.IsNullOrWhiteSpace(line))
+            {
+                Console.Write("Enter segment #" + (list.Count + 1) + " : ");
+                line = Console.ReadLine();
+                list.Add(line + " ");
+            }
+
+            Console.WriteLine();
+
+            var certManager = new CertificateManager(path);
+            var password = certManager.PermutatePasswords(list.ToArray());
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                Console.WriteLine("password not found");
+            } 
+            else
+            {
+                Console.WriteLine("password found : " + password);
+            }
+        }
+    }
+}
